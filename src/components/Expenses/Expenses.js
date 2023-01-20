62cca35c40e82fd7d287bbbfa09ceb76c3397793
Kristina <kris.kartashova@gmail.com>
2023-01-20 15:54:35 +0200

    add a chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,6 +5,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, addFilteredYear] = useState("2000");
@@ -25,6 +26,7 @@ function Expenses(props) {
           selected={filteredYear}
           addFiltedYear={filterDateHahdler}
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses}/>
       </Card>
     </li>
