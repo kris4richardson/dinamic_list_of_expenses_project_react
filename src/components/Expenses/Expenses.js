994957a75ff92d8c21f102451f0d9940c93a9ecb
Kristina <kris.kartashova@gmail.com>
2023-01-20 13:32:26 +0200

    conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,33 +1,33 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
+
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
-
-  const [filteredYear, addFilteredYear] = useState('2000');
+  const [filteredYear, addFilteredYear] = useState("2000");
 
   const filterDateHahdler = (selectedYear) => {
     addFilteredYear(selectedYear);
-    
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+ 
   return (
-    <div>
+    <li>
       <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} addFiltedYear={filterDateHahdler} />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesFilter
+          selected={filteredYear}
+          addFiltedYear={filterDateHahdler}
+        />
+        <ExpensesList items={filteredExpenses}/>
       </Card>
-    </div>
+    </li>
   );
 }
 
